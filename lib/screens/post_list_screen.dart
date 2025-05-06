32fae831446a57336053999b582d42e3c353f774
subeeen16 <sajagombi@gmail.com>
2025-05-07 01:33:21 +0900

    게시물 UI 초안 구성

--- lib/screens/post_list_screen.dart
+++ lib/screens/post_list_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
+
 class PostListScreen extends StatelessWidget {
+  final String username = 'UNI_SEOUL1234'; // 임시 닉네임(UNI+지역명+난수)
+
   const PostListScreen({super.key});
 
   @override
@@ -9,9 +13,43 @@ class PostListScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('게시물'),
       ),
-      body: const Center(
-        child: Text('게시물 기능 준비 중...'),
+      body: ListView.builder(
+        itemCount: 3, // 임시 게시글 3개
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: const CircleAvatar(
+                    backgroundColor: Colors.blueAccent,
+                    child: Icon(Icons.person),
+                  ),
+                  title: Text(username),
+                  subtitle: Text( '게시글입니다'),
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.orangeAccent, // 나중에 이미지로 대체 가능
+                  child: const Center(child: Text('사진 영역 (지도 이미지 대체 예정)')),
+                ),
+                ButtonBar(
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.favorite_border),
+                      label: const Text('좋아요'),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
 }
+
+
